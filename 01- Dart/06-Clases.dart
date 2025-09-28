548c671c5034365b66b6fdd92169675021d16085
esteban8356 <99091517+esteban8356@users.noreply.github.com>
2025-09-28 11:35:08 -0500

    Create 06-Clases.dart
    corregir nombre

---
+++ 01- Dart/06-Clases.dart
@@ -0,0 +1,26 @@
+void main(){
+  
+  final Hero wolverine = Hero(name:'Logan',power:'Regeneration');
+  
+  print (wolverine);
+  print (wolverine.name);
+  print (wolverine.power);
+
+}
+
+
+class Hero {
+  String name;
+  String power;
+  
+  Hero({ 
+    required this.name, 
+    this.power = 'Sin Poder'
+    });
+  
+  @override
+  String toString(){
+    return '$name - $power';
+  }
+
+}
